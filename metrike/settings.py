4d3af4102239ea8629aeb68e97cd6a994f85bbcb
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2021-05-10 15:24:36 -0300

    first commit

--- metrike/settings.py
+++ metrike/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 
-# import django_heroku
-import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,7 +132,3 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-
-
-# Configure Django App for Heroku.
-django_heroku.settings(locals())
